20cbabfb6003fd8326c3afd9bdfe22c15a4e5312
Tenshi4D <158043079+Tenshi4D@users.noreply.github.com>
2024-02-01 16:00:01 +0000

    Cacca

--- cazzo.c
+++ cazzo.c
@@ -7,16 +7,21 @@ int main()
 
 
     char x[4] = {'y', 'e', 's', '\0'};
+    char y[3] = {'n', 'o', '\0'};
     char answer[4];
-
+    int budget;
     printf("This code will tell you where you must go on vacation.\nJust answer this simple questionnaire and you will know where to travel for your next trip!\n");
-
+    
+    printf("What is your budget: €");
+    scanf("%d", &budget);
+    if(budget > 1500)
+    
+    
     printf("Do you want to go to the beach?\n");
     scanf("%s", answer);
 
-    if (strcmp(answer, x) == 0) {
-        printf("utgv\n");
-    }
+    if (answer)
+    }else if
 
     return 0;
 }
